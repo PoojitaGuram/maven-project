6adb4545c6477988bca9a9eecf312df09ccfe9dc
pooji <pooji@DESKTOP-IVV148L>
2023-06-05 23:28:06 +0530

    maven project to github

---
+++ MAVEN/src/test/java/testcases/Testcase1.java
@@ -0,0 +1,12 @@
+package testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+	@Test
+public void testcase1() {
+	
+	Reporter.log("execute the testcases");
+}
+}
